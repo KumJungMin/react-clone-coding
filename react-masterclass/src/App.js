771567edf92d3522f7be3b75d343f4a787a03cb7
KumJungMin <rmawjdals@naver.com>
2023-10-23 22:55:49 +0900

    1. styled-components는 스타일을 입힌 컴포넌트를 쉽게 만들 수 있다

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,8 +1,32 @@
+import styled from "styled-components";
+
+// styled-components는 클래스명을 자동으로 생성해준다.
+// styled.html_요소명`스타일` 형식으로 사용한다.
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
+const BoxTwo = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+`;
+const Text = styled.span`
+  color: blue;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Father>
+      <BoxOne>
+        <Text>안녕!</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
