548d32802583191bfaa43c383fcff5a9e8c48188
KumJungMin <rmawjdals@naver.com>
2023-10-30 14:24:42 +0900

    4. useRef로 ref를 만들어 html요소에 접근 가능

--- my-app/src/components/Board.tsx
+++ my-app/src/components/Board.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DragabbleCard from "./DragabbleCard";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 300px;
@@ -43,8 +44,18 @@ interface IBoardProps {
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  // ref는 HTML 요소를 지정하고, 가져올 수 있는 방법
+  // useRef는 .current 프로퍼티로 전달된 인자(initialValue)를 가진,
+  //  변경 가능한 ref 객체를 반환
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+  };
   return (
     <Wrapper>
+      <input ref={inputRef} />
+      <button onClick={onClick}>추가</button>
+
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
         {(magic, info) => (
