3dc1db9589755b38952f040b61a759efe31bd0a9
KumJungMin <rmawjdals@naver.com>
2023-10-30 13:18:52 +0900

    1. recoil로 전역변수 선언하는 법

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,29 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState, hourSelector } from "./atoms";
+
 function App() {
-  return <div>1</div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+
+  function onMinuteChange(e: React.FormEvent<HTMLInputElement>) {
+    const value = +e.currentTarget.value;
+    setMinutes(value);
+  }
+  return (
+    <div>
+      <input
+        type="number"
+        value={minutes}
+        placeholder="Minute"
+        onChange={onMinuteChange}
+      />
+      <input
+        type="number"
+        value={hours}
+        placeholder="hours"
+      />
+    </div>
+  );
 }
 
 export default App;
