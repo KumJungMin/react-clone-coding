3af3703dbb1d2324cc4f6ce89166eab4ed1f6465
KumJungMin <rmawjdals@naver.com>
2023-11-04 13:06:25 +0900

    chore: 기본 설정하기

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { RecoilRoot } from "recoil";
-// RecoilRoot이란, react-recoil을 사용하기 위한 컴포넌트이다.
+import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-// react-query를 사용하기 위해 QueryClientProvider를 적용한다.
 const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
