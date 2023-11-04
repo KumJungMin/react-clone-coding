6092a8e45c8dc2d46dc16c089d8710481bd05414
KumJungMin <rmawjdals@naver.com>
2023-11-04 13:16:26 +0900

    1. Route로 라우터 설정하기

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Tv from "./routes/Tv";
+
+// BrowserRouter는 HTML5의 History API를 사용하여 주소를 관리한다.
+// Switch는 한번에 오직 하나의 Route만 Render하게 해준다.
+// Route는 path에 해당하는 컴포넌트를 렌더링한다.
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
